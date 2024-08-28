8e33568610bd0ab44d089ef76c95cc6652eec70e
GivaldoEc <givaldo.t.santos@gmail.com>
2024-08-28 15:27:29 -0300

    makefile configurado

---
+++ build/obj/chobjcaches.d
@@ -0,0 +1,137 @@
+build/obj/chobjcaches.o: \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/src/chobjcaches.c \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/ch.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/license/chlicense.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/license/chversion.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/license/chcustomer.h \
+ cfg/chconf.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chchecks.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chrestrictions.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chearly.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/common/ports/AVR/compilers/GCC/chtypes.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chrfcu.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chdebug.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chtime.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chlists.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chalign.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chtrace.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chport.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/common/ports/AVR/chcore.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/osal/rt-nil/chcore_timer.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chtm.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chstats.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chobjects.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chsys.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chinstances.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chvt.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chschd.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chthreads.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chregistry.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chsem.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chmtx.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chcond.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chevents.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chmsg.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chlib.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chbsem.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmboxes.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmemcore.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmemheaps.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmempools.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chobjfifos.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chpipes.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chobjcaches.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chdelegates.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chjobs.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chfactory.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chdynamic.h
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/ch.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/license/chlicense.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/license/chversion.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/license/chcustomer.h:
+
+cfg/chconf.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chchecks.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chrestrictions.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chearly.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/common/ports/AVR/compilers/GCC/chtypes.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chrfcu.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chdebug.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chtime.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chlists.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chalign.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chtrace.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chport.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/common/ports/AVR/chcore.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/osal/rt-nil/chcore_timer.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chtm.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chstats.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chobjects.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chsys.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chinstances.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chvt.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chschd.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chthreads.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chregistry.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chsem.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chmtx.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chcond.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chevents.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chmsg.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chlib.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chbsem.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmboxes.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmemcore.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmemheaps.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmempools.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chobjfifos.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chpipes.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chobjcaches.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chdelegates.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chjobs.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chfactory.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chdynamic.h:
---
+++ build/obj/hal_pal_lld.d
@@ -0,0 +1,277 @@
+build/obj/hal_pal_lld.o: \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/ports/AVR/MEGA/LLD/GPIOv1/hal_pal_lld.c \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/common/portability/GCC/ccportab.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/osal/rt-nil/osal.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/ch.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/license/chlicense.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/license/chversion.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/license/chcustomer.h \
+ cfg/chconf.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chchecks.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chrestrictions.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chearly.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/common/ports/AVR/compilers/GCC/chtypes.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chrfcu.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chdebug.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chtime.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chlists.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chalign.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chtrace.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chport.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/common/ports/AVR/chcore.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/osal/rt-nil/chcore_timer.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chtm.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chstats.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chobjects.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chsys.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chinstances.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chvt.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chschd.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chthreads.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chregistry.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chsem.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chmtx.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chcond.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chevents.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chmsg.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chlib.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chbsem.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmboxes.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmemcore.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmemheaps.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmempools.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chobjfifos.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chpipes.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chobjcaches.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chdelegates.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chjobs.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chfactory.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chdynamic.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/boards/ARDUINO_NANO/board.h \
+ cfg/halconf.h cfg/mcuconf.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/ports/AVR/MEGA/ATMEGAxx/hal_lld.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_objects.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_streams.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_channels.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_files.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_ioblock.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_mmcsd.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_persistent.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_flash.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_buffers.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_queues.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_buffered_serial.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_pal.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/ports/AVR/MEGA/LLD/GPIOv1/hal_pal_lld.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/ports/AVR/MEGA/ATMEGAxx/avr_pins.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_adc.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_can.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_crypto.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_dac.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_efl.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_gpt.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_i2c.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_i2s.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_icu.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_mac.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_pwm.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_rtc.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_serial.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/ports/AVR/MEGA/LLD/USARTv1/hal_serial_lld.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_sdc.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_sio.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_spi.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_trng.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_uart.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_usb.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_wdg.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_wspi.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_st.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/ports/AVR/MEGA/LLD/SYSTICKv1/hal_st_lld.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_mmc_spi.h \
+ /home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_serial_usb.h
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/common/portability/GCC/ccportab.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/osal/rt-nil/osal.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/ch.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/license/chlicense.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/license/chversion.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/license/chcustomer.h:
+
+cfg/chconf.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chchecks.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chrestrictions.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chearly.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/common/ports/AVR/compilers/GCC/chtypes.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chrfcu.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chdebug.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chtime.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chlists.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chalign.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chtrace.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chport.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/common/ports/AVR/chcore.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/osal/rt-nil/chcore_timer.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chtm.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chstats.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chobjects.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chsys.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chinstances.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chvt.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chschd.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chthreads.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chregistry.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chsem.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chmtx.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chcond.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chevents.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chmsg.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chlib.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chbsem.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmboxes.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmemcore.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmemheaps.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chmempools.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chobjfifos.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chpipes.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chobjcaches.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chdelegates.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chjobs.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/oslib/include/chfactory.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/rt/include/chdynamic.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/boards/ARDUINO_NANO/board.h:
+
+cfg/halconf.h:
+
+cfg/mcuconf.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/ports/AVR/MEGA/ATMEGAxx/hal_lld.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_objects.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_streams.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_channels.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_files.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_ioblock.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_mmcsd.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_persistent.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_flash.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_buffers.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_queues.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_buffered_serial.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_pal.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/ports/AVR/MEGA/LLD/GPIOv1/hal_pal_lld.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/ports/AVR/MEGA/ATMEGAxx/avr_pins.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_adc.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_can.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_crypto.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_dac.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_efl.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_gpt.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_i2c.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_i2s.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_icu.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_mac.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_pwm.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_rtc.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_serial.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/ports/AVR/MEGA/LLD/USARTv1/hal_serial_lld.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_sdc.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_sio.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_spi.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_trng.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_uart.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_usb.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_wdg.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_wspi.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_st.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/ports/AVR/MEGA/LLD/SYSTICKv1/hal_st_lld.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_mmc_spi.h:
+
+/home/givaldo/Documents/ChibiStudio/chibios_stable-21.11.x/os/hal/include/hal_serial_usb.h:
